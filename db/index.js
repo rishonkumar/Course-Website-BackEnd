233fb5d4fa98671a45055597066180fe397dcf12
rishonkumar <rishon.kumar.rahi@gmail.com>
2023-08-08 19:29:20 +0530

    Added DB structure and made few changes

--- db/index.js
+++ db/index.js
@@ -3,20 +3,33 @@ const mongoose = require("mongoose")
 //schema define
 
 
-const signupSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+    email: String,
+    username: String,
+    password: String,
+    purchasedCourse: [{type: mongoose.Schema.Types.ObjectId, ref: "Course"}]
+})
+
+const adminSchema = new mongoose.Schema({
     username: String,
     password: String
 })
 
-const loginSchema = new mongoose.Schema({
-    username: String,
-    password: String
+const courseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    tag: String,
+    price: Number,
+    imgLink: String,
+    published: Boolean
 })
 
-const Sign = mongoose.model("Sign", signupSchema);
-const Login = mongoose.model("Login", loginSchema);
+const User = mongoose.model("Sign", userSchema);
+const Admin = mongoose.model("Login", adminSchema);
+const Course = mongoose.model("Course", courseSchema)
 
 module.exports = {
-    Sign,
-    Login
+    User,
+    Admin,
+    Course
 }
